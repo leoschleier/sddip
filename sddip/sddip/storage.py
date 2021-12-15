02e1e85c9daf90cda338cd8da98d719d14252fa5
Leo Schleier <leo.schleier@gmail.com>
2021-12-15 12:10:23 +0100

    Add cut coefficients

--- sddip/sddip/storage.py
+++ sddip/sddip/storage.py
@@ -13,6 +13,10 @@ class SolutionStorage:
     def get_solution(self, iteration_index: int, sample_index: int, stage_index: int):
         return self.solutions[iteration_index, sample_index, stage_index]
 
+    def get_stage_solutions(self, stage_index: int):
+        df = self.to_dataframe()
+        return df.query("t == %i"%(stage_index)).to_dict("list")
+
     def to_dataframe(self):
         df = pd.DataFrame.from_dict(self.solutions, orient="index")
         return df.rename_axis(self.index_names)
